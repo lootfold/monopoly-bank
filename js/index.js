d5250d764aeef3543d0d93452894636b563acf46
Pallav Dubey <pallav_92@hotmail.com>
2021-11-27 00:06:29 -0700

    update index page

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-let balanceForm;
-let noOfPlayersForm;
-let playersForm;
-
 // object to store game data
 data = {
   noOfPlayers: 0,
@@ -15,102 +11,126 @@ window.onload = handleWindowLoad;
 function handleWindowLoad() {
   // get form ref & add event handlers for forms
 
-  balanceForm = document.getElementById("form_bank_balance");
-  balanceForm.onsubmit = handleBalanceFormSubmit;
+  const balanceForm = document.forms.gameDetails;
+  balanceForm.onsubmit = handleGameDetailsSubmit;
 
-  noOfPlayersForm = document.getElementById("form_no_of_players");
-  noOfPlayersForm.onsubmit = handleNoOfPlayersFormSubmit;
-
-  playersForm = document.getElementById("form_players_names");
+  const playersForm = document.forms.playersForm;
   playersForm.onsubmit = handlePlayersFormSubmit;
-}
 
-function handleBalanceFormSubmit() {
-  // store balance input to game data
-  data.banksBalance = parseInt(balanceForm.bank_balance.value);
-  data.eachPlayerStartingBalance = parseInt(balanceForm.player_balance.value);
+  addEventListenersForValidation(balanceForm);
 
-  // display form to enter no of players
-  noOfPlayersForm.classList.remove("hidden");
-  return false;
-}
+  function addEventListenersForValidation(form) {
+    const elements = form.elements;
 
-function handleNoOfPlayersFormSubmit() {
-  // store no of players to game data
-  data.noOfPlayers = parseInt(noOfPlayersForm.no_of_players.value);
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      if (element.type !== "submit") {
+        // show error style when user leaves the input field
+        element.onblur = validateField;
 
-  // display form to input player details
-  displayFieldsForPlayers();
+        // remove error style if the value is valid as the user is typing
+        element.onkeyup = clearErrorStyle;
+      }
+    }
 
-  return false;
-}
+    function validateField() {
+      if (this.checkValidity()) {
+        this.classList.remove("error");
+      } else {
+        this.classList.add("error");
+      }
+    }
 
-function handlePlayersFormSubmit() {
-  // get ref to all player elements
-  const elements = playersForm.elements;
-
-  // loop through form elements and store player name with balance to game data
-  for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-
-    // get name form text input
-    if (element.type === "text") {
-      data.players.push({
-        name: element.value,
-        balance: data.eachPlayerStartingBalance,
-      });
+    function clearErrorStyle() {
+      if (this.checkValidity()) {
+        this.classList.remove("error");
+      }
     }
   }
 
-  // save game data in session storage
-  sessionStorage.setItem("data", JSON.stringify(data));
+  function handleGameDetailsSubmit() {
+    // store balance input to game data
+    data.banksBalance = parseInt(balanceForm.bankBalance.value);
+    data.eachPlayerStartingBalance = parseInt(balanceForm.playerBalance.value);
+    data.noOfPlayers = parseInt(balanceForm.noOfPlayers.value);
 
-  // navigate to banker page
-  window.location.href = "banker.html";
+    // display form to get player details
+    displayFieldsForPlayers();
 
-  return false;
-}
-
-function displayFieldsForPlayers() {
-  // display form for player names
-  const playersForm = document.getElementById("form_players_names");
-  playersForm.classList.remove("hidden");
-
-  // clear all fields in the form
-  const playersInputContainer = document.getElementById(
-    "player_inputs_container"
-  );
-  playersInputContainer.innerHTML = "";
-
-  // generate new fileds based on the input & add to page
-  for (let i = 1; i <= data.noOfPlayers; i++) {
-    const field = generatePlayerInput(i);
-    playersInputContainer.appendChild(field);
+    return false;
   }
-}
 
-function generatePlayerInput(playerNo) {
-  const idStr = "player";
-  const inputDiv = document.createElement("div");
+  function handlePlayersFormSubmit() {
+    // get ref to all player elements
+    const elements = playersForm.elements;
 
-  // create label
-  const inputLabel = document.createElement("label");
-  inputLabel.setAttribute("for", `${idStr}_${playerNo}}`);
-  inputLabel.innerText = `Player ${playerNo}`;
+    // loop through form elements and store player name with balance to game data
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
 
-  // create text field for player name
-  const inputField = document.createElement("input");
-  inputField.id = `${idStr}_${playerNo}}`;
-  inputField.name = `${idStr}_${playerNo}}`;
-  inputField.type = "text";
-  inputField.required = true;
-  inputField.minLength = 3;
-  inputField.maxLength = 20;
+      // get name form text input
+      if (element.type === "text") {
+        data.players.push({
+          name: element.value,
+          balance: data.eachPlayerStartingBalance,
+        });
+      }
+    }
 
-  // add fields to div & apply style
-  inputDiv.appendChild(inputLabel);
-  inputDiv.appendChild(inputField);
-  inputDiv.classList.add("form-group");
+    // save game data in session storage
+    sessionStorage.setItem("data", JSON.stringify(data));
 
-  return inputDiv;
+    // navigate to banker page
+    window.location.href = "banker.html";
+
+    return false;
+  }
+
+  function displayFieldsForPlayers() {
+    // display form for player names
+    playersForm.classList.remove("hidden");
+
+    // clear all fields in the form
+    const playersInputContainer = document.getElementById(
+      "playersFormInputContainer"
+    );
+    playersInputContainer.innerHTML = "";
+
+    // generate new fileds based on the input & add to page
+    for (let i = 1; i <= data.noOfPlayers; i++) {
+      const field = generatePlayerInput(i);
+      playersInputContainer.appendChild(field);
+    }
+
+    addEventListenersForValidation(playersForm);
+  }
+
+  function generatePlayerInput(playerNo) {
+    const idStr = "player";
+    const inputDiv = document.createElement("div");
+    inputDiv.classList.add("form__group");
+
+    // create label
+    const inputLabel = document.createElement("label");
+    inputLabel.setAttribute("for", `${idStr}_${playerNo}}`);
+    inputLabel.innerText = `Player ${playerNo}`;
+    inputLabel.classList.add("form__label");
+
+    // create text field for player name
+    const inputField = document.createElement("input");
+    inputField.id = `${idStr}_${playerNo}}`;
+    inputField.name = `${idStr}_${playerNo}}`;
+    inputField.type = "text";
+    inputField.required = true;
+    inputField.minLength = 3;
+    inputField.maxLength = 20;
+    inputField.classList.add("form__control");
+
+    // add fields to div & apply style
+    inputDiv.appendChild(inputLabel);
+    inputDiv.appendChild(inputField);
+    inputDiv.classList.add("form-group");
+
+    return inputDiv;
+  }
 }
